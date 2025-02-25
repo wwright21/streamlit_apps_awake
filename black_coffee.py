8ddbd9b779d149b998a688f01ca0cc5068d47a74
arc-research-analytics <wwright@atlantaregional.org>
2025-02-25 10:20:24 -0500

    added apps to automation script

---
+++ black_coffee.py
@@ -0,0 +1,30 @@
+from playwright.sync_api import sync_playwright
+
+streamlit_apps = [
+    "https://test-app-247.streamlit.app/",
+    "https://baldwin-county-migration.streamlit.app/",
+    "https://nashville-dash.streamlit.app/",
+    "https://kolter-nashville.streamlit.app/",
+]
+
+
+def awake_app(app_url):
+    with sync_playwright() as p:
+        # Set headless=True to run in the background
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+
+        # 60s timeout to handle loading
+        page.goto(app_url, wait_until="networkidle", timeout=60000)
+
+        browser.close()
+
+
+# Awake all apps in the list
+def awake_all_apps():
+    for app in streamlit_apps:
+        awake_app(app)
+
+
+if __name__ == "__main__":
+    awake_all_apps()
