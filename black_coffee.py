b2428cc7cb21a8e967ef05fe7589d96aa6ca42e2
arc-research-analytics <wwright@atlantaregional.org>
2025-02-25 14:26:05 -0500

    updated app

--- black_coffee.py
+++ black_coffee.py
@@ -1,5 +1,4 @@
 from playwright.sync_api import sync_playwright
-from datetime import datetime
 
 streamlit_apps = [
     "https://test-app-247.streamlit.app/",
@@ -26,8 +25,6 @@ def awake_app(app_url):
 def awake_all_apps():
     for app in streamlit_apps:
         awake_app(app)
-    with open("wake_streamlit_log.txt", "a") as f:
-        f.write("Streamlit apps woke up successfully at {}\n".format(datetime.now()))
 
 
 if __name__ == "__main__":
